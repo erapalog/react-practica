85261a513138834b821adfc7153a0420fbae2027
erapalog <erobertrapalo@hotmail.com>
2024-03-18 22:18:44 -0600

    proyecto base

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,16 @@
+//import './App.css'
+
+import { NavBar } from './components/NavBar'
+import './css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <h3>Hola</h3>
+    </>
+  )
+}
+
+export default App
